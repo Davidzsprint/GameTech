b42c5341a7f132251c52afeca2f119f8c6e84a9b
Davidzsprint <60217867+Davidzsprint@users.noreply.github.com>
2020-02-18 22:50:30 +0100

    Create Ranking.java

---
+++ GameTech/src/main/java/com/gametech/demo/Ranking.java
@@ -0,0 +1,54 @@
+package com.example.GameTech;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Ranking {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@OneToMany
+	private List<Usuario> usuarios = new ArrayList<Usuario>();
+	
+	public Ranking () {}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public void addUsuairo(Usuario usuario) {
+		this.usuarios.add(usuario);
+	}
+	
+	public void monstrarUsuariosRanking() {
+		
+		System.out.println("Lista de usuarios del Ranking");
+		System.out.println("------------------------------");
+		for(Usuario u : this.usuarios) {
+			System.out.println(u.getAlias());
+		}
+	}
+	
+	
+}
