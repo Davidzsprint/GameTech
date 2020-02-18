a69ea73ece9a6e568743e287e5dbb2be0192a691
rubembs7 <61190606+rubembs7@users.noreply.github.com>
2020-02-18 23:06:40 +0100

    Update DataBaseUsage.java

--- GameTech/src/main/java/com/gametech/demo/DataBaseUsage.java
+++ GameTech/src/main/java/com/gametech/demo/DataBaseUsage.java
@@ -1,1 +1,21 @@
+package com.example.GameTech;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DataBaseUsage implements CommandLineRunner{
+	
+	//Inyectamos los repositorios
+	
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+	
+		//Invertamos los datos predeficinidos en la BBDD.
+		
+	}
+
+}
 
