f76d3198a225f070a10fb68f36c0a5318383292a
calderon95 <47868021+calderon95@users.noreply.github.com>
2020-02-18 23:44:23 +0100

    Modify Heroe

--- GameTech/src/main/java/com/gametech/demo/Heroe.java
+++ GameTech/src/main/java/com/gametech/demo/Heroe.java
@@ -1,4 +1,4 @@
-package com.gametech.demo;
+package com.example.GameTech;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,18 +19,12 @@ public class Heroe {
 	private int ataque;
 	private int defensa;
 	private int mana;
-	private int dificultad;
+	private String dificultad;
 	
+	public Heroe() {}
 	
-	public Heroe() {
+	public Heroe(String nombre, String descripcion, String posicion, int vida, int ataque, int defensa, int mana, String dificultad) {
 		
-	}
-	
-	
-	public Heroe(long id, String nombre, String descripcion, String posicion, int vida, int ataque, int defensa,
-			int mana, int dificultad) {
-		super();
-		this.id = id;
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 		this.posicion = posicion;
@@ -40,74 +34,89 @@ public class Heroe {
 		this.mana = mana;
 		this.dificultad = dificultad;
 	}
-
-
-
+	
+	
+	
 	public long getId() {
 		return id;
 	}
+	
 	public void setId(long id) {
 		this.id = id;
 	}
+	
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	public String getDescripcion() {
 		return descripcion;
 	}
+	
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+	
 	public String getPosicion() {
 		return posicion;
 	}
+	
 	public void setPosicion(String posicion) {
 		this.posicion = posicion;
 	}
+	
 	public int getVida() {
 		return vida;
 	}
+	
 	public void setVida(int vida) {
 		this.vida = vida;
 	}
+	
 	public int getAtaque() {
 		return ataque;
 	}
+	
 	public void setAtaque(int ataque) {
 		this.ataque = ataque;
 	}
+	
 	public int getDefensa() {
 		return defensa;
 	}
+	
 	public void setDefensa(int defensa) {
 		this.defensa = defensa;
 	}
+	
 	public int getMana() {
 		return mana;
 	}
+	
 	public void setMana(int mana) {
 		this.mana = mana;
 	}
 	
-	
-	public int getDificultad() {
+	public String getDificultad() {
 		return dificultad;
 	}
-
-
-	public void setDificultad(int dificultad) {
+	
+	public void setDificultad(String dificultad) {
 		this.dificultad = dificultad;
 	}
-
-
+	
+	
 	@Override
 	public String toString() {
-		return "Heroe [nombre=" + nombre + ", descripcion=" + descripcion + ", posicion=" + posicion
-				+ ", vida=" + vida + ", ataque=" + ataque + ", defensa=" + defensa + ", mana=" + mana + "]";
+		return "Heroe [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", posicion=" + posicion
+				+ ", vida=" + vida + ", ataque=" + ataque + ", defensa=" + defensa + ", mana=" + mana + ", dificultad="
+				+ dificultad + "]";
 	}
+	
 	
 	
 	
