a9d417429383488acd9115b85ca057fb4c8df1bd
rubembs7 <61190606+rubembs7@users.noreply.github.com>
2020-03-04 23:10:05 +0100

    Add files via upload

--- GameTech/src/main/java/com/gametech/demo/DataBaseUsage.java
+++ GameTech/src/main/java/com/gametech/demo/DataBaseUsage.java
@@ -1,21 +1,254 @@
-package com.example.GameTech;
+package com.example.HolaMundo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 @Controller
-public class DataBaseUsage implements CommandLineRunner{
+public class DataBaseUsage implements CommandLineRunner {
 	
-	//Inyectamos los repositorios
+	@Autowired
+	private AlumnoRepository reposotorioAlumno;
 	
+	@Autowired
+	private AsignaturaRepository repositorioAsignatura;
+	
+	@Autowired
+	private HeroeRepository repositorioHeroe;
+	
+	@Autowired
+	private UsuarioRepository repositorioUsuario;
+	
+	@Autowired
+	private BuildRepository repositorioBuild;
+	
+	@Autowired
+	private RankingRepository repositorioRanking;
+	
+	@Autowired
+	private LigaRepository repositorioLiga;
 
 	@Override
 	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		/**
+		reposotorioAlumno.save(new alumno("Beltran", 27));
+		reposotorioAlumno.save(new alumno("Beltran", 27));
+		reposotorioAlumno.save(new alumno("Cerezo", 30));
+		
+		
+		repositorioAsignatura.save(new asignatura("Programacion", "Dificil"));
+		repositorioAsignatura.save(new asignatura("BBDD", "Facil"));
+		repositorioAsignatura.save(new asignatura("SSOO", "Dificil"));
+		//--------------------------------------------------------------------------------------------------------------------------------------------------
+		//Creo los hereoes
+		Heroe garen = new Heroe("Garen", "Ideal para tanquear gracias a su alta recuperacion de vida", "Top", 900, 400, 500, 600, "Facil");
+		Heroe alistar = new Heroe("Alistar", "Toro con una fuerza sobrenatural", "Support", 1000, 100, 700, 600, "Media");
+		Heroe elise = new Heroe("Elise", "Mitad araña mitad humana, es venenosa", "Jungla", 600, 600, 350, 500, "Dificil");
+		Heroe ashe = new Heroe("Ashe", "Arquera que con punteria exqusita, >---|>", "ADC", 400, 900, 250, 400, "Facil");
+		Heroe nami = new Heroe("Nami", "Es una sirena que maneja el agua con destreza", "Support", 400, 600, 400, 500, "Media");
+		Heroe akali = new Heroe("Akali", "Ninja escurridiza, cada de desaparecer en cualquer instante", "Mid", 600, 600, 500, 650, "Media");
+		
+		repositorioHeroe.save(garen);
+		repositorioHeroe.save(alistar);
+		repositorioHeroe.save(elise);
+		repositorioHeroe.save(ashe);
+		repositorioHeroe.save(nami);
+		repositorioHeroe.save(akali);
+		
+		//Creo los Usuarios
+		Usuario ruben = new Usuario("Rubew", "qwerty", "Spain", "EUW", "rubenlolgmail.com");
+		Usuario david = new Usuario("Draky", "qwerty1", "Spain", "EUW", "davidlolgmail.com");
+		Usuario calderon = new Usuario("Dacal", "ytrewq", "Spain", "Korea", "dacalgmail.com");
+		Usuario juli = new Usuario("Juli", "1234", "Spain", "Korea", "juli@gmail.com");
+		Usuario pan = new Usuario("Pam", "4321", "Korea", "Kore", "pangmail.com");
+		
+		
+		
+		//Creo las Builds
+		Build b1 = new Build("ADC", "Filo infinito", "Doran", "Botas berserker", "hoja del rey", "Sanguinaria", "Segador de ensencias");
+		Build b2 = new Build("Support", "Hombreras de acero", "Chaleco de cadenas", "Botas de velocidad", "Cristal de rubi", "Gema avivadora", "Guardian");
+		Build b3 = new Build("AP", "Doran", "Pocion de vida", "Botas de hechicero", "Baston del vacio", "Vara explosiva", "Sombrero mortal");
+		Build b4 = new Build("Jungla", "Tabi de ninja", "Daga dentada", "Filoscuro", "Encantamiento guerrero", "Guardian", "Lente del oraculo");
+		Build b5 = new Build("Tank", "Cuchilla negra", "Calibrador de sterak", "Rostro espiritual", "Malla de guardian", "Guardian", "Angel de la guardia");
+		Build b6 = new Build("Healer", "Daga de hechicero", "Pocion de vida", "Redencion", "Warmong", "Medallon de solari", "Botas jonias");
+		
+		
+		
+		//Me creo una lista de Buils y añado las buids anterior a la lista
+		List<Build> listaGuias = new ArrayList<>();
+		listaGuias.add(b1);
+		listaGuias.add(b2);
+		listaGuias.add(b3);
+	
+
+		repositorioUsuario.save(ruben);
+		repositorioUsuario.save(david);
+		repositorioUsuario.save(calderon);
+		repositorioUsuario.save(juli);
+		repositorioUsuario.save(pan);
+		
+		//Las guardo/inserto las Build en la BBDD con su correspondiente repositorio
+		repositorioBuild.save(b1);
+		repositorioBuild.save(b2); 
+		repositorioBuild.save(b3);
+		repositorioBuild.save(b4);
+		repositorioBuild.save(b5);
+		repositorioBuild.save(b6);
+		
+		
+		
+		//le paso la lista con las Buils de cada usuario
+		ruben.setBuilds(listaGuias);
+		pan.setBuilds(listaGuias);
+
+		//Las guardo/inserto los Usuarios en la BBDD con su correspondiente repositorio
+		repositorioUsuario.save(ruben);
+		//repositorioUsuario.save(david);
+		//repositorioUsuario.save(calderon);
+		
+		//Me creo el Ranking y añado los usuarios a lista de usuarios.
+		Ranking ranking = new Ranking();
+		ranking.addUsuairo(ruben);
+		ranking.addUsuairo(david);
+		ranking.addUsuairo(calderon);
+		
+		//Inserto el ranking en la BD
+		repositorioRanking.save(ranking);
+		
+		
+		//Me creo las 4 Ligas  (Bronce,  plata, oro y diamante)
+		Liga bronce = new Liga("Bronce");
+		Liga plata = new Liga("Plata");
+		Liga oro = new Liga("Oro");
+		Liga diamante = new Liga("Diamante");
+		Liga platino = new Liga("Platino");
+		
+		
+		
+		bronce.addUsuarioLiga(calderon);
+		oro.addUsuarioLiga(ruben);
+		oro.addUsuarioLiga(david);
+		platino.addUsuarioLiga(pan);
+		diamante.addUsuarioLiga(juli);
+		
+		repositorioLiga.save(bronce);
+		repositorioLiga.save(plata);
+		repositorioLiga.save(oro);
+		repositorioLiga.save(diamante);
 		
 	
-		//Invertamos los datos predeficinidos en la BBDD.
+		 */
+		
+		
+		List<alumno> alumnos = reposotorioAlumno.findAll();
+		System.out.println("Alumnos encontrados con findAll:");
+		System.out.println("---------------------------------");
+		System.out.println(alumnos);
+		System.out.println("");
+		
+		List<alumno> Beltran = reposotorioAlumno.findByNombre("Beltran");
+		System.out.println("Alumnos encontrados con Beltran:");
+		System.out.println("---------------------------------");
+		for(alumno b : Beltran) { 
+			System.out.println(b);
+		}
+		
+		System.out.println("");
+		List<asignatura> dificultad = repositorioAsignatura.findByDificultad("Dificil");
+		System.out.println("Asignaturas con dificultad Dificil:");
+		System.out.println("-----------------------------------");
+		for(asignatura d : dificultad) {
+			System.out.println(d);
+		}
+		
+		
+		System.out.println("");
+		List<Heroe> heroe = repositorioHeroe.findByPosicion("ADC");
+		System.out.println("Hereoes ADCs:");
+		System.out.println("-----------------------------------");
+		for(Heroe d : heroe) {
+			System.out.println(d.getNombre());
+		}
+		
+		List<Usuario> usu = repositorioUsuario.findAll();
+		System.out.println("Usuario");
+		System.out.println("-----------------------------------");
+		for(Usuario lista : usu) { 
+			System.out.println(lista.getAlias());
+		}
+		
+		List<Usuario> usu1 = repositorioUsuario.findByPaisOrderByEloDesc("Spain");
+		System.out.println("\nRANKING\nUsuario      ELO");
+		System.out.println("-----------------------------------");
+		for(Usuario lista : usu1) { 
+			System.out.print(lista.getAlias()+"       [");
+			System.out.println(lista.getElo()+"]");
+		}
+		
+		
+		System.out.println("\nEncontrar liga Oro");
+		Liga l = repositorioLiga.findByNombreLiga("Oro");
+		System.out.println(l.toString());
+		System.out.println(l.getId());
+		//List<Usuario> p =l.getUsuarios();
+		//for(Usuario u : p) {
+			//System.out.println(u.getAlias());
+		//}
+		
+		Usuario ususs = repositorioUsuario.findByAlias("Rubew");
+		System.out.println(ususs.getContrasena());
+		String e = ususs.getEmail();
+		System.out.println(e);
+	
+		
+		Build guias = repositorioBuild.findById(18);
+		System.out.println(guias.toString());
+		
+		
+		Build builds = repositorioBuild.findByNombreBuild("ADC");
+		System.out.println(builds.getNombreBuild());
+		
+				
+
+		
+		/**
+		System.out.println("\nEncontrar Usuario por Alias  = Rubew");
+		Usuario u = repositorioUsuario.findByAlias("Rubew");
+		if(!(u.getAlias() == null)) {
+			System.out.println(u);
+		}
+		
+		
+		System.out.println("\nEncontrar Usuario por Contraseña  = ytrewq");
+		Usuario u1 = repositorioUsuario.findByContrasena("ytrewq");
+		if(!(u1.getContrasena() == null)) {
+			System.out.println(u1);
+		}
+		
+		
+		if(u.getAlias() != null && u1.getContrasena() != null) {
+			
+			System.out.println("Encontado!!");
+		}
+		
+		*/
 		
 	}
-
+	
+	@GetMapping("/felicidades")
+	public String pagheroe(Model model) {
+		model.addAttribute("name", repositorioHeroe.findAll());
+		return "felicidades";
+		
+	}		
+		
 }
-
---
+++ GameTech/src/main/java/com/gametech/demo/controladores.java
@@ -0,0 +1,327 @@
+package com.example.HolaMundo;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.apache.catalina.authenticator.SpnegoAuthenticator.AuthenticateAction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@ComponentScan(basePackages = { "com.example.HolaMundo" })
+public class controladores implements CommandLineRunner {
+
+	@Autowired
+	private AlumnoRepository reposotorioAlumno;
+
+	@Autowired
+	private AsignaturaRepository repositorioAsignatura;
+
+	@Autowired
+	private HeroeRepository repositorioHeroe;
+
+	@Autowired
+	private UsuarioRepository repositorioUsuario;
+
+	@Autowired
+	private BuildRepository repositorioBuild;
+
+	@Autowired
+	private RankingRepository repositorioRanking;
+
+	@Autowired
+	private LigaRepository repositorioLiga;
+
+	@RequestMapping("/index")
+	public String inicio() {
+		return "index";
+	}
+
+	@RequestMapping("/realizarconsultas")
+	public String realizarconsultas() {
+		return "realizarconsultas";
+	}
+	
+	@RequestMapping("/areaprivada")
+	public String areaprivada() {
+		return "areaprivada";
+	}
+	
+	
+	@RequestMapping("/registrarusuario")
+	public String registrarUsuario() {
+		return "registrousuario";
+	}
+	
+	@RequestMapping("/areagestionusuario")
+	public String areagestionUsuario() {
+		return "areagestionusuario";
+	}
+	
+	@RequestMapping("/registrarbuild")
+	public String registrarBuild() {
+		return "registrarbuild";
+	}
+	
+	@RequestMapping("/registrarheroe")
+	public String registrarHeroe() {
+		return "registrarHeroe";
+	}
+	
+	@RequestMapping("/informacionusuario")
+	public String informacionUsuario() {
+		return "informacionusuario";
+	}
+	
+	// Buscar por Usuario
+
+	@GetMapping("/consultarusuario/alias")
+	public String verUsuario(Model model, @RequestParam String alias) {
+
+		// Comprobamos si existe o no un Usuario registrado con ese "alias".
+		Usuario usuario = repositorioUsuario.findByAlias(alias);
+
+		// Si existe, mostramos la información.
+		if (usuario != null) {
+
+			model.addAttribute("usuario", usuario);
+
+			return "consultausuario";
+		} else {
+			// Si no existe notifica el error al usuario.
+			return "errorusuarionoexiste";
+		}
+	}
+
+	// BUSCAR POR NOMBRE HEROE
+	@GetMapping("/consultarheroe/nombre")
+	public String verHeroe(Model model, @RequestParam String nombre) {
+
+		// Comprobamos si existe o no un Heroe registrado con ese "nombre".
+		Heroe heroe = repositorioHeroe.findByNombre(nombre);
+
+		// Si existe, mostramos la información.
+		if (heroe != null) {
+
+			model.addAttribute("heroe", heroe);
+
+			return "consultaheroe";
+
+		} else {
+			// Si no existe notifica el error al usuario.
+			return "errorusuarionoexiste";
+		}
+	}
+
+	// BUSCAR HEROE POR DIFICULTAD
+	@GetMapping("/consultardificultadheroe/dificultad")
+	public String verHeroeDificultad(Model model, @RequestParam String dificultad) {
+
+		// Comprobamos si existe o no un Heroe registrado con esa "dificultad".
+		List<Heroe> existe = repositorioHeroe.findByDificultad(dificultad);
+
+		if (existe.isEmpty()) {
+
+			// Si no existe notifica el error al usuario.
+			return "errorusuarionoexiste";
+
+		} else {
+
+			List<Heroe> heroe = repositorioHeroe.findByDificultad(dificultad);
+
+			model.addAttribute("heroe", heroe);
+
+			return "consultadificultadheroe";
+		}
+	}
+
+	// BUSCAR POR NOMBRE BUILD
+	@GetMapping("/consultarbuild/nombreBuild")
+	public String verBuild(Model model, @RequestParam String nombreBuild) {
+
+		// Comprobamos
+		Build build = repositorioBuild.findByNombreBuild(nombreBuild);
+
+		// Si existe, mostramos la información.
+		if (build != null) {
+
+			model.addAttribute("build", build);
+
+			return "consultabuild";
+
+		} else {
+			// Si no existe notifica el error al usuario.
+			return "errorusuarionoexiste";
+		}
+	}
+
+	// Buscar USUARIO EN RANKING
+	@GetMapping("/consultarranking/pais")
+	public String verRanking(Model model, @RequestParam String pais) {
+
+		// Comprobamos si existe o no un Usuario registrado con ese "alias".
+		List<Usuario> existe = repositorioUsuario.findByPais(pais);
+		
+
+		// Si existe, mostramos la información.
+		if (existe != null) {
+			
+			List<Usuario> usuario = repositorioUsuario.findByPaisOrderByEloDesc(pais);
+			model.addAttribute("usuario", usuario);
+
+			return "consultaranking";
+		} else {
+			// Si no existe notifica el error al usuario.
+			return "errorusuarionoexiste";
+		}
+	}
+	
+	
+	// Buscar USUARIO POR PAIS
+	@GetMapping("/consultarnacionalidadusuario/pais")
+	public String verNacionalidad(Model model, @RequestParam String pais) {
+
+		// Comprobamos si existe o no un Usuario registrado con ese "alias".
+		List<Usuario> existe = repositorioUsuario.findByPais(pais);
+		
+
+		// Si existe, mostramos la información.
+		if (existe != null) {
+			
+			List<Usuario> usuario = repositorioUsuario.findByPais(pais);
+			model.addAttribute("usuario", usuario);
+
+			return "consultanacionalidadusuario";
+		} else {
+			// Si no existe notifica el error al usuario.
+			return "errorusuarionoexiste";
+		}
+	}
+	
+	
+
+	// ---------------------------------------------------------------------------------------------------
+	// REGISTRO    USUARIO Y   BUILDS
+	// ---------------------------------------------------------------------------------------------------
+	
+	@PostMapping("/nuevoUsuario")
+	public String registroUsuario(@RequestParam String alias, @RequestParam String contrasena, @RequestParam String pais, @RequestParam String servidor, @RequestParam String email) {
+		
+		//comprobamos si existe ese usuairio
+		Usuario existe = repositorioUsuario.findByAlias(alias);
+		
+		if(existe == null) {
+			
+			Usuario usuario = new Usuario(alias, contrasena, pais, servidor, email);
+			repositorioUsuario.save(usuario);
+			
+			return "areaprivada";
+		}
+		else 
+		{
+			return "errorregistro";
+		}
+	
+	}
+	
+	@PostMapping("/nuevoBuild")
+	public String registroBuild(@RequestParam String nombreBuild, @RequestParam String objeto1, @RequestParam String objeto2, @RequestParam String objeto3, @RequestParam String objeto4, @RequestParam String objeto5, @RequestParam String objeto6) {
+		
+		//comprobamos si existe esa build
+		Build existe = repositorioBuild.findByNombreBuild(nombreBuild);
+		
+		if(existe == null) {
+			
+			Build build = new Build(nombreBuild, objeto1, objeto2, objeto3, objeto4, objeto5, objeto6);
+			repositorioBuild.save(build);
+			
+			return "areagestionusuario";
+		}
+		else 
+		{
+			return "errorregistrobuild";
+		}
+	}
+	
+	@PostMapping("/nuevoHeroe")
+	public String registroHeroe(@RequestParam String nombre, @RequestParam String descripcion, @RequestParam String posicion, @RequestParam int vida, @RequestParam int ataque, @RequestParam int defensa, @RequestParam int mana, @RequestParam String dificultad) {
+		
+		Heroe existe = repositorioHeroe.findByNombre(nombre);
+		
+		
+		if(existe == null) {
+			
+			Heroe heroe = new Heroe(nombre, descripcion, posicion, vida, ataque, defensa, mana, dificultad);
+			repositorioHeroe.save(heroe);
+			
+			return "areagestionusuario";
+		}
+		else {
+			
+			
+			return "errorregistrarheroe";
+		}
+	}
+	
+	@RequestMapping(value = "/mostrarHeroe")
+	public String registroUsuario(@RequestParam String nombre) {
+		return "";
+
+	}
+
+	
+	//PARA SIGUENTE FASE	
+	@RequestMapping("/login")
+	public String loginUsuario(Model model, @RequestParam String alias, @RequestParam String contrasena,
+			HttpSession sesision) {
+
+		Usuario usu = repositorioUsuario.findByAlias(alias);
+		Usuario usu2 = repositorioUsuario.findByContrasena(contrasena);
+
+		// Comprobmaos que el alias y la contaseña no son Null
+		if (!(usu.getAlias() == null)) {
+			if (usu.getContrasena().equals(contrasena)) {
+
+				System.out.println("Usuario logueado correcamente!");
+
+				return ("index");
+
+			}
+		}
+		// Si no existe que se registre
+		return ("registroUsuario");
+	}
+
+	@GetMapping("/hello")
+	public String despedida(Model model) {
+
+		model.addAttribute("name", reposotorioAlumno.findAll());
+		return "hello1";
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @GetMapping("/hello") public String despedida(Model model) {
+	 * model.addAttribute("name", "Gilipollas"); return "hello"; }
+	 * 
+	 * @GetMapping("/felicidades") public String felicitar(Model model) {
+	 * model.addAttribute("nombre", "Rubew"); return "felicidades"; }
+	 * 
+	 */
+
+}
